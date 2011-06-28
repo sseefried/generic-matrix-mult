4ec950fe78b3d81aac0dd3903ad97cb05854b28c
Sean Seefried <sean.seefried@gmail.com>
2011-06-28 15:35:34 +1000

    Cleaned up definitions. More point-free.

--- instances-inc.hs
+++ instances-inc.hs
@@ -1,15 +1,15 @@
 instance (Functor (Enc (TYPE sh)), EncodeF (TYPE sh)) => Functor (TYPE sh) where
-  fmap f a = decode (fmap f (encode a))
+  fmap f = decode . fmap f . encode
 
 instance (Foldable (Enc (TYPE sh)), EncodeF (TYPE sh)) => Foldable (TYPE sh) where
-  fold ma = fold (encode ma)
+  fold = fold . encode
 
 instance (Applicative (Enc (TYPE sh)), EncodeF (TYPE sh)) => Applicative (TYPE sh) where
-  pure a = decode (pure a)
+  pure     = decode . pure
   fa <*> a = decode (encode fa <*> encode a)
 
 instance (Traversable (Enc (TYPE sh)), EncodeF (TYPE sh)) => Traversable (TYPE sh) where
-  traverse f x = fmap decode (traverse f (encode x))
+  traverse f = fmap decode . traverse f . encode
 
 instance (Replicate (Enc (TYPE sh)), EncodeF (TYPE sh)) => Replicate (TYPE sh) where
-  replicate a = decode (replicate a)
+  replicate = decode . replicate
